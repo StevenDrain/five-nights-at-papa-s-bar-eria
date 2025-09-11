da8d8abf850ae99e225a35c84a7af58ec9d6cc48
StevenDrain <stevendrain@u.boisestate.edu>
2025-09-11 12:49:19 -0600

    formatted a bit

--- Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Activator.cs
+++ Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Activator.cs
@@ -8,6 +8,8 @@ public class Activator : MonoBehaviour
     public InputActionReference key;
     GameObject note;
     public SphereCollider[] Activators;
+    public int activeTime = 3;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -19,35 +21,43 @@ public class Activator : MonoBehaviour
     {
         KeyRead();
     }
-    
+
     void KeyRead()
     {
         Vector2 keyInput = key.action.ReadValue<Vector2>();
 
         if (keyInput.y > 0)
         {
+            
             SphereCollider aKey = Activators[0];
             Debug.Log(aKey.name);
             aKey.enabled = true;
-
+            
         }
         else if (keyInput.y < 0)
         {
+
             SphereCollider bKey = Activators[1];
             Debug.Log(bKey.name);
             bKey.enabled = true;
+            
         }
         else if (keyInput.x > 0)
         {
+
             SphereCollider cKey = Activators[2];
             Debug.Log(cKey.name);
             cKey.enabled = true;
+            
         }
         else if (keyInput.x < 0)
         {
+            
             SphereCollider switcherKey = Activators[3];
             Debug.Log(switcherKey.name);
             switcherKey.enabled = true;
+            
         }
     }
+    
 }
