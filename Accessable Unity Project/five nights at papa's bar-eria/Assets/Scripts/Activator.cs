3961980e946aaeff6bc480d334121019174e53f6
StevenDrain <stevendrain@u.boisestate.edu>
2025-09-29 15:38:52 -0600

    Revert "finished a part of rhythm"
    This reverts commit 5102c0cb32d899456684dec14298c2e735afbe95.

--- Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Activator.cs
+++ Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Activator.cs
@@ -3,16 +3,11 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Controls;
 public class Activator : MonoBehaviour
 {
     public InputActionReference key;
     GameObject note;
-    public GameObject[] Activators;
-    private Material baseColor;
-
-    public Material color;
-   
+    public SphereCollider[] Activators;
 
     // Start is called before the first frame update
     void Start()
@@ -33,75 +28,40 @@ public class Activator : MonoBehaviour
         if (keyInput.y > 0)
         {
 
-
-            GameObject aKey = Activators[0];
-
-            SphereCollider aKeyCol = aKey.GetComponent<SphereCollider>();
-
-            aKeyCol.enabled = true;
-
-            GameObject aKeyObject = Activators[0].GameObject();
-
-            Renderer aKeyRenderer = aKeyObject.GetComponent<Renderer>();
-
-            aKeyRenderer.material.SetColor("_Color", Color.yellow);
-
+            SphereCollider aKey = Activators[0];
+            
+            aKey.enabled = true;
 
         }
         else if (keyInput.y < 0)
         {
 
-            GameObject bKey = Activators[1];
-
-            SphereCollider bKeyCol = bKey.GetComponent<SphereCollider>();
-
-            bKeyCol.enabled = true;
-
-            Renderer bKeyRenderer = bKey.GetComponent<Renderer>();
-
-            bKeyRenderer.material.SetColor("_Color", Color.cyan);
+            SphereCollider bKey = Activators[1];
+            
+            bKey.enabled = true;
 
         }
         else if (keyInput.x > 0)
         {
 
-            GameObject cKey = Activators[2];
+            SphereCollider cKey = Activators[2];
 
-            SphereCollider cKeyCol = cKey.GetComponent<SphereCollider>();
-
-            cKeyCol.enabled = true;
-
-            Renderer cKeyRenderer = cKey.GetComponent<Renderer>();
-
-            cKeyRenderer.material.SetColor("_Color", Color.green);
+            cKey.enabled = true;
 
         }
         else if (keyInput.x < 0)
         {
 
-            GameObject switcherKey = Activators[3];
-
-            SphereCollider switcherKeyCol = switcherKey.GetComponent<SphereCollider>();
-
-            switcherKeyCol.enabled = true;
-
-            Renderer switcherKeyRenderer = switcherKey.GetComponent<Renderer>();
-
-            switcherKeyRenderer.material.SetColor("_Color", Color.blue);
+            SphereCollider switcherKey = Activators[3];
+            
+            switcherKey.enabled = true;
 
         }
         else
         {
-            foreach (GameObject keyObject in Activators)
+           foreach (SphereCollider keyCollider in Activators)
             {
-                SphereCollider keyCollider = keyObject.GetComponent<SphereCollider>();
                 keyCollider.enabled = false;
-
-                Renderer keyRenderer = keyObject.GetComponent<Renderer>();
-                if (keyRenderer != null)
-                {
-                    keyRenderer.material.SetColor("_Color", Color.white);
-                }
             }
         }
     }
