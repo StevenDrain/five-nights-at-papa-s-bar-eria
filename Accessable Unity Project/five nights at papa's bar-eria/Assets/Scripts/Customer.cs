aae16048a93448bd9b289433436c89e0971af38e
LukasHall1 <61564157+LukasHall1@users.noreply.github.com>
2025-09-29 14:57:30 -0600

    fix

--- Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Customer.cs
+++ Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/Customer.cs
@@ -69,6 +69,9 @@ public class Customer : MonoBehaviour
 
         if (beerRequested == true)
         {
+            if(served == true) {
+                
+            }
             return;
         }
         else
