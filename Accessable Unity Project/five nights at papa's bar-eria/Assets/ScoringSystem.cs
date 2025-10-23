d859b6209a533ec51b48ec0a52710b0996b9f9f5
StevenDrain <stevendrain@u.boisestate.edu>
2025-10-23 11:02:32 -0600

    added scoring

--- Accessable Unity Project/five nights at papa's bar-eria/Assets/ScoringSystem.cs
+++ Accessable Unity Project/five nights at papa's bar-eria/Assets/ScoringSystem.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using System;
+
 
 public class ScoringSystem : MonoBehaviour
 {
     public int mulitiplierScore;
+    public int regScore;
+    public int score;
+    private string srtingScore;
+
+    public TMP_Text text;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +22,11 @@ public class ScoringSystem : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        Debug.Log(mulitiplierScore);
+    { 
+        score = mulitiplierScore + regScore;
+        srtingScore = score.ToString();
+        text.text = "Score: " + srtingScore;
+        ;
     }
    public void KeyDestroyed()
     {
