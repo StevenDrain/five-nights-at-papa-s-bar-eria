fb0a591d24652f937d82097d6b6dd732fd8f05fa
StevenDrain <stevendrain@u.boisestate.edu>
2025-12-04 20:57:05 -0700

    Added Start Screen
    Finished Start Screen

---
+++ Accessable Unity Project/five nights at papa's bar-eria/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public string MainSceneName;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(MainSceneName);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
